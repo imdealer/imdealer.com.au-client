d7179035dd0c22ac7ea37315a8f55d100518b448
Hyun Ho Oh <hyunhooh0413@gmail.com>
2019-02-21 19:08:29 +1100

    20190221190828

---
+++ vserver/gulpfile.js
@@ -0,0 +1,71 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
+const cleanCss = require('gulp-clean-css');
+const useref = require('gulp-useref');
+const gulpif = require('gulp-if');
+const del = require('del');
+const uglify = require('gulp-uglify');
+const imagemin = require('gulp-imagemin');
+const cache = require('gulp-cache');
+
+function syncBrowser() {
+  browserSync.init({
+    server: {
+      baseDir: './src'
+    }
+  });
+}
+
+function compileSass() {
+  return gulp.src('./src/scss/**/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./src/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
+}
+
+function reloadingHtml() {
+  return gulp.src('./src/*.html').pipe(browserSync.reload({
+    stream: true
+  }));
+}
+
+function reloadingJs() {
+  return gulp.src('./src/js/**/*.js').pipe(browserSync.reload({
+    stream: true
+  }));
+}
+
+function watchFiles() {
+  gulp.watch('./src/scss/**/*.scss', gulp.series(compileSass));
+  gulp.watch('./src/*.html', gulp.series(reloadingHtml));
+  gulp.watch('./src/js/**/*.js', gulp.series(reloadingJs));
+}
+
+function userefTask() {
+  return gulp.src('./src/*.html')
+    .pipe(useref())
+    .pipe(gulpif('*.css', cleanCss()))
+    .pipe(gulpif('*.js', uglify()))
+    .pipe(gulp.dest('./dist'));
+}
+
+function copyFonts() {
+  return gulp.src('./src/fonts/**/*').pipe(gulp.dest('./dist/fonts'));
+}
+
+function cleanDist(cb) {
+  del.sync('./dist');
+  cb();
+}
+
+function imageProcessing() {
+  return gulp.src('./src/images/**/*')
+    .pipe(cache(imagemin({interlaced: true})))
+    .pipe(gulp.dest('./dist/images'));
+}
+
+exports.default = gulp.parallel(syncBrowser, compileSass, watchFiles);
+exports.build = gulp.series(cleanDist, gulp.parallel(userefTask, copyFonts, imageProcessing));
