a4601b5e864bd5cf729f92486c20cbaa899e7c5a
"NaraeH" <"NaraeH"@NARAE-PC>
2019-02-22 17:38:23 +1000

    Cookie 관리 페이지

--- vserver/src/js/pages/Showroom.js
+++ vserver/src/js/pages/Showroom.js
@@ -4,4 +4,12 @@ function Showroom () {
 
 Showroom.prototype.render = function () {
   console.log('Showroom.prototype.render');
+
+  //쿠키 생성 테스트
+  Cookie.setCookie(Cookie.CAR_ID, "carIdTest");
+  console.log(Cookie.getCookie(Cookie.CAR_ID));
+  
+  //쿠키 삭제 테스트
+  Cookie.deleteCookie(Cookie.CAR_ID);
+  console.log(Cookie.getCookie(Cookie.CAR_ID));
 };
